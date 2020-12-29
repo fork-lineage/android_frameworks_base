60b2227d14fad16fe800a57da4a8cd5e26988a29
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-12-29 14:05:01 +0530

    BatteryService: add Turbo power charging support
    Inspired by oneplus dash charging commit
    
    https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    
    Charlie-117: Port to R
    
    Signed-off-by: Charlie-117 <gmoto1179@gmail.com>
    Change-Id: I12784d37062753a306af9fd9c9d5cbba0cc578ea

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -196,6 +196,9 @@ public final class BatteryService extends SystemService {
     private boolean mVoocCharger;
     private boolean mLastVoocCharger;
 
+    private boolean mTurboPower;
+    private boolean mLastTurboPower;
+
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
 
@@ -599,6 +602,7 @@ public final class BatteryService extends SystemService {
         mDashCharger = isDashCharger();
         mWarpCharger = isWarpCharger();
         mVoocCharger = isVoocCharger();
+        mTurboPower = isTurboPower();
 
         if (force || (mHealthInfo.batteryStatus != mLastBatteryStatus ||
                 mHealthInfo.batteryHealth != mLastBatteryHealth ||
@@ -614,6 +618,7 @@ public final class BatteryService extends SystemService {
                 mDashCharger != mLastDashCharger ||
                 mWarpCharger != mLastWarpCharger ||
                 mVoocCharger != mLastVoocCharger ||
+                mTurboPower != mLastTurboPower ||
                 mBatteryModProps.modLevel != mLastModLevel ||
                 mBatteryModProps.modStatus != mLastModStatus ||
                 mBatteryModProps.modFlag != mLastModFlag ||
@@ -792,6 +797,7 @@ public final class BatteryService extends SystemService {
             mLastDashCharger = mDashCharger;
             mLastWarpCharger = mWarpCharger;
             mLastVoocCharger = mVoocCharger;
+            mLastTurboPower = mTurboPower;
             mLastModLevel = mBatteryModProps.modLevel;
             mLastModStatus = mBatteryModProps.modStatus;
             mLastModFlag = mBatteryModProps.modFlag;
@@ -827,6 +833,7 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_DASH_CHARGER, mDashCharger);
         intent.putExtra(BatteryManager.EXTRA_WARP_CHARGER, mWarpCharger);
         intent.putExtra(BatteryManager.EXTRA_VOOC_CHARGER, mVoocCharger);
+        intent.putExtra(BatteryManager.EXTRA_TURBO_POWER, mTurboPower);
         intent.putExtra(BatteryManager.EXTRA_MOD_LEVEL, mBatteryModProps.modLevel);
         intent.putExtra(BatteryManager.EXTRA_MOD_STATUS, mBatteryModProps.modStatus);
         intent.putExtra(BatteryManager.EXTRA_MOD_FLAG, mBatteryModProps.modFlag);
@@ -923,6 +930,20 @@ public final class BatteryService extends SystemService {
             br.close();
             file.close();
             return "1".equals(state);
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+        return false;
+    }
+
+    private boolean isTurboPower() {
+        try {
+            FileReader file = new FileReader("/sys/class/power_supply/battery/charge_rate");
+            BufferedReader br = new BufferedReader(file);
+            String state = br.readLine();
+            br.close();
+            file.close();
+            return "Turbo".equals(state);
         } catch (FileNotFoundException e) {
         } catch (IOException e) {
         }
