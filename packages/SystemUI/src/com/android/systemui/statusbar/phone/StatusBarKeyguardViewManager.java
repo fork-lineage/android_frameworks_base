77dbf61f888cdba161433be9fcb46a152520a7be
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-30 20:01:01 +1100

    Partially revert "Wake up to lock screen when bouncer is visible"
    Commit 3cfd6889cfcb9cb271aa758b6a43d2186ddaab85 causes the screen to
    randomly turn back on right after being turned off when a biometric
    unlock method is enabled. When this occurs, the following message is
    left in logcat:
    
    I PowerManagerService: Waking up from Dozing (uid=10188, reason=WAKE_REASON_GESTURE, details=com.android.systemui:BOUNCER_VISIBLE)...
    
    This occurs even though no gesture has taken place. This can be
    especially annoying and even a potential security hazard when face
    unlock is used, as the screen turning back on engages the face unlock
    process and can unlock the device if the user's face is still visible
    to the device.
    
    Change-Id: I3550d9531cfc97be50747c6460c9df237d23b5fb
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -127,8 +127,6 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         @Override
         public void onFullyShown() {
             updateStates();
-            mStatusBar.wakeUpIfDozing(SystemClock.uptimeMillis(),
-                    mStatusBar.getBouncerContainer(), "BOUNCER_VISIBLE");
             onKeyguardBouncerFullyShownChanged(true);
             showFaceRecognizingMessage();
         }
