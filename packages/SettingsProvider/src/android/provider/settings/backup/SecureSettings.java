ad7ef1fdf7361796e862ec66e6cdeb45e222c7b1
palaych <palaych@arrowos.net>
2022-02-07 03:53:25 +0000

    Add monet tunable keys into backup too
    Change-Id: I5fa31380204260369b2b184904005d359ee991e8

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -195,6 +195,12 @@ public class SecureSettings {
         Settings.Secure.LOCKSCREEN_SHOW_WALLET,
         Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK,
         Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON,
-        Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE
+        Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
+        Settings.Secure.MONET_ENGINE_ACCURATE_SHADES,
+        Settings.Secure.MONET_ENGINE_COLOR_OVERRIDE,
+        Settings.Secure.MONET_ENGINE_CUSTOM_COLOR,
+        Settings.Secure.MONET_ENGINE_CHROMA_FACTOR,
+        Settings.Secure.MONET_ENGINE_LINEAR_LIGHTNESS,
+        Settings.Secure.MONET_ENGINE_WHITE_LUMINANCE,
     };
 }
