6cc5640d049568eb2595c83e5ff3185f506ab558
sairam1411 <majgaonkars3@gmail.com>
2022-03-06 16:13:14 +0530

    fixup!: Implement edge long swipe gesture [1/3]
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>
    Change-Id: Ifda04778c9bf8780f77c53bfceaee48238e569d0

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
@@ -56,7 +56,7 @@ import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
 import com.android.systemui.plugins.NavigationEdgeBackPlugin;
 import com.android.systemui.shared.navigationbar.RegionSamplingHelper;
-//import com.android.systemui.statusbar.VibratorHelper;
+import com.android.systemui.statusbar.VibratorHelper;
 
 import java.io.PrintWriter;
 import java.util.concurrent.Executor;
@@ -135,7 +135,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
             = new PathInterpolator(1.0f / RUBBER_BAND_AMOUNT_APPEAR, 1.0f, 1.0f, 1.0f);
 
     private final WindowManager mWindowManager;
-//    private final VibratorHelper mVibratorHelper;
+    private final VibratorHelper mVibratorHelper;
 
     /**
      * The paint the arrow is drawn with
@@ -286,7 +286,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         super(context);
 
         mWindowManager = context.getSystemService(WindowManager.class);
-//        mVibratorHelper = Dependency.get(VibratorHelper.class);
+        mVibratorHelper = Dependency.get(VibratorHelper.class);
 
         mDensity = context.getResources().getDisplayMetrics().density;
 
